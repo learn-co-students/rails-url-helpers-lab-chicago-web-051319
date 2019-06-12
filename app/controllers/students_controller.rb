ac0348785cade4431bc2d89be27541da69d56a55
github username <github email address>
2019-06-12 10:22:59 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,24 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @status = Student.find(params[:id]).active
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    current_status = @student.active
+    @student.update(active: !current_status)
+
+    @student.save
+    #@student.active == true ? @student.active = false : @student = true
+    # if @student.active == true
+    #   @student.active = false
+    # else
+    #   @student.active = true
+    # end
+
+    puts "===================== Setting #{@student.to_s} 'active' status from: #{current_status} to: #{@student.active}"
+    redirect_to student_path(@student)
   end
 
   private
